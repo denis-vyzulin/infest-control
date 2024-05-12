24db6f54ac7b51c13293b15807838e85f3168af9
Adrimmel <147333825+Adrimmel@users.noreply.github.com>
2024-05-12 11:58:47 +0300

    Update Saving.cpp

--- classes/Saving.cpp
+++ classes/Saving.cpp
@@ -1,38 +1,8 @@
 // ================================ //
 // == КЛАСС СИМУЛЯЦИИ ПРОГРЕССА == //
 // ================================ //
-#ifndef EPIDEMIC_SIMULATION_H
-#define EPIDEMIC_SIMULATION_H
 
-#include <iostream>
-#include <vector>
-#include <fstream>
-#include "Desease.h"
-#include "Measure.h"
-
-class EpidemicSimulation {
-public:
-EpidemicSimulation();
-
-void simulate(const Virus& virus, const Measure& measure, int duration);
-
-private:
-int day;
-
-void applyMeasures(const Virus& virus, const Measure& measure);
-
-void updateEpidemic(Epidemic& epidemic, const Virus& virus);
-
-void saveSimulation(const std::vector<Epidemic>& epidemics);
-};
-
-#endif // EPIDEMIC_SIMULATION_H
-```
-
-**epidemic_simulation.cpp**
-
-```cpp
-#include "epidemic_simulation.h"
+#include "Saving.h"
 
 EpidemicSimulation::EpidemicSimulation() {
 day = 0;
@@ -103,4 +73,3 @@ std::cout « "Симуляция сохранена в файле simulation.txt
 std::cout « "Ошибка при сохранении симуляции" « std::endl;
 }
 }
-#include <iostream>
