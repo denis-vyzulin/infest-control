a4e882ccc9b180dcf7f2c81222bfd35c682b9728
Adrimmel <147333825+Adrimmel@users.noreply.github.com>
2024-05-12 11:47:25 +0300

    Update Measure.cpp

--- classes/Measure.cpp
+++ classes/Measure.cpp
@@ -1,5 +1,29 @@
 // =========================== //
-// == КЛАСС ПРОТИВОДЕЙСТВИЙ == //
+// == КЛАСС ПРОТИВОДЕЙСТВИЙ РЕАЛИЗАЦИЯ == //
 // =========================== //
+#include "measure.h"
 
+void Measure::setStatus(const bool status) {
+    _status = status;
+}
+
+int Measure::getStatus() {
+    return _status;
+}
+
+void Measure::setRates(const std::vector<int>& rates) {
+    _rates = rates;
+}
+
+void Measure::getRates(std::vector<int>& rates) {
+    rates = _rates;
+}
+
+void Measure::setSeasonality(const std::vector<int>& seasonality) {
+    _seasonality = seasonality;
+}
+
+void Measure::getSeasonality(std::vector<int>& seasonality) {
+    seasonality = _seasonality;
+}
 #include <iostream>
